7b5e26f0ed9dfa65573350f7c98bf8196d76078a
diego gonzalez <diego.gonzalez-g@mail.escuelaing.edu.co>
2022-03-21 22:56:16 -0500

    Pagina de inicio y redireccion por JS

--- src/main/java/edu/escuelaing/arsw/App.java
+++ src/main/java/edu/escuelaing/arsw/App.java
@@ -1,13 +1,18 @@
 package edu.escuelaing.arsw;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+@SpringBootApplication
+@ComponentScan(basePackages = {"edu.eci.arsw.collabpaint"})
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
