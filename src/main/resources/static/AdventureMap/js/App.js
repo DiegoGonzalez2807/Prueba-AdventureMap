6194ed22c9c1530cb64b8921e8e4eb35a5a3cd43
eduardoospina <eduardo.ospina@mail.escuelaing.edu.co>
2022-04-01 21:46:57 -0500

    heroku n d

--- src/main/resources/static/AdventureMap/js/App.js
+++ src/main/resources/static/AdventureMap/js/App.js
@@ -7,6 +7,7 @@
     var count = 1;
     var h1;
     var h2;
+    const url5 = 'http://adventuremap.herokuapp.com/AdventureMap';
     /**
      * Funcion generada para redireccionar desde la página inicial
      * a la página donde se encuentra el mapa. Se recibe el nombre
@@ -16,7 +17,7 @@
     function redirect(data){
         name = data;
         console.log(name);
-        window.location = "../AdventureMap/Mapa.html"
+        window.location = "url5/AdventureMap/Mapa.html"
     }
 
     // /**
